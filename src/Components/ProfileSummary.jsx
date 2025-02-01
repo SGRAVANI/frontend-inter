bd7a42c8bfb61e479860cd4a4492547d1b08c87b
SGRAVANI <shakuntla.ravani@gmail.com>
2025-02-01 18:29:59 +0530

    updated

--- src/Components/ProfileSummary.jsx
+++ src/Components/ProfileSummary.jsx
@@ -4,13 +4,15 @@ import { endpoint } from "../data";
 
 function ProfileSummary() {
   const [pdata, setPdata] = useState({});
-
+  const [loading,setIsloading]=useState(false)
   async function getData() {
     try {
+        setIsloading(true)
       let res = await fetch(`${endpoint}/personal`);
       let data = await res.json();
       setPdata(data.data[0]);
       console.log(data.data[0]);
+      setIsloading(false)
     } catch (e) {
       console.error("Failed to fetch data", e);
     }
@@ -22,16 +24,17 @@ function ProfileSummary() {
 
   return (
     <div className="max-w-md w-full mx-auto mt-10 p-6 bg-white shadow-lg rounded-xl text-center">
+        {loading?<div>data is loading...</div>:<>
       <div className="mb-4">
         <img
-          src="/sgr.jpeg"
+          src={pdata.img}
           alt="Profile"
           className="w-24 h-24 mx-auto rounded-full border-2 border-gray-300"
         />
       </div>
       <h2 className="text-xl font-semibold text-gray-800">{pdata.name || "N/A"}</h2>
       <p className="text-gray-600">{pdata.email || "N/A"}</p>
-      <p className="mt-3 text-gray-700 text-sm">{pdata.profile_summary || "No summary available."}</p>
+      <p className="mt-3 text-gray-700 text-sm">{pdata.profile_summary || "No summary available."}</p></> }
     </div>
   );
 }
