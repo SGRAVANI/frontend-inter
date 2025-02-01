516676f312058efb942ea57e3e59c6d54ef5692e
SGRAVANI <shakuntla.ravani@gmail.com>
2025-02-01 19:37:57 +0530

    updated

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Certificates() {
   return (
-    <div className='mt-50 text-center'>Certificates</div>
+    <div className='mt-[50px] text-center'>Certificates</div>
   )
 }
 
